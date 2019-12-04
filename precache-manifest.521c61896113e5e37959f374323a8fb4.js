e41ade3c191f70636ef69e8ab7eab78fad5a6cf0
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2019-12-04 15:43:55 -0500

    Updates

---
+++ precache-manifest.521c61896113e5e37959f374323a8fb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a724f572ad4c26a2881f8947d7ffbefa",
+    "url": "/borrowFrontEnd/index.html"
+  },
+  {
+    "revision": "4d35ebd0dabf4c301450",
+    "url": "/borrowFrontEnd/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "d72d63b65463de3ef5aa",
+    "url": "/borrowFrontEnd/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "4d35ebd0dabf4c301450",
+    "url": "/borrowFrontEnd/static/js/2.2ac8e907.chunk.js"
+  },
+  {
+    "revision": "d72d63b65463de3ef5aa",
+    "url": "/borrowFrontEnd/static/js/main.6aeded7c.chunk.js"
+  },
+  {
+    "revision": "cb98e02b862522cb0092",
+    "url": "/borrowFrontEnd/static/js/runtime-main.af580747.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/borrowFrontEnd/static/media/logo.25bf045c.svg"
+  }
+]);
